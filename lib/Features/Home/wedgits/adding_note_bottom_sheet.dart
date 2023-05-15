462fb1bdc534c2ee3628b7c90682791e19b44160
AhmedAwadNosir <ahmedalprens98@gmail.com>
2023-05-16 01:20:34 +0300

    validate data that user  can,t give me empty notes

--- lib/Features/Home/wedgits/adding_note_bottom_sheet.dart
+++ lib/Features/Home/wedgits/adding_note_bottom_sheet.dart
@@ -1,66 +1,97 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'color_picker.dart';
 import 'custom_text_form_field.dart';
 
-class AddNoteBottomSheet extends StatelessWidget {
+class AddNoteBottomSheet extends StatefulWidget {
   const AddNoteBottomSheet({
     super.key,
   });
 
+  @override
+  State<AddNoteBottomSheet> createState() => _AddNoteBottomSheetState();
+}
+
+String? title;
+String? subTitle;
+GlobalKey<FormState> formKey = GlobalKey();
+
+class _AddNoteBottomSheetState extends State<AddNoteBottomSheet> {
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding:
           EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
       child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const CustomTextFormField(
-              hint: "learn Flutter",
-            ),
-            const CustomTextFormField(
-              hint: "learn Flutter With tharwat samy",
-              maxlines: 5,
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const [
-                  ColorPiker(color: Colors.white),
-                  ColorPiker(color: Colors.red),
-                  ColorPiker(color: Colors.lightBlue),
-                  ColorPiker(color: Colors.green),
-                ],
+        child: Form(
+          key: formKey,
+          autovalidateMode: autovalidateMode,
+          child: Column(
+            children: [
+              CustomTextFormField(
+                hint: "learn Flutter",
+                onSaved: (value) {
+                  title = value;
+                  log(title!);
+                },
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.cyan,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10))),
-                  child: const Padding(
-                    padding: EdgeInsets.all(7),
-                    child: Text(
-                      "Add",
-                      style: TextStyle(fontSize: 30, color: Colors.black),
+              CustomTextFormField(
+                hint: "learn Flutter With tharwat samy",
+                maxlines: 5,
+                onSaved: (value) {
+                  subTitle = value;
+                  log(subTitle!);
+                },
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    ColorPiker(color: Colors.white),
+                    ColorPiker(color: Colors.red),
+                    ColorPiker(color: Colors.lightBlue),
+                    ColorPiker(color: Colors.green),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        formKey.currentState!.save();
+                      } else {
+                        autovalidateMode = AutovalidateMode.always;
+                        setState(() {});
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.cyan,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    child: const Padding(
+                      padding: EdgeInsets.all(7),
+                      child: Text(
+                        "Add",
+                        style: TextStyle(fontSize: 30, color: Colors.black),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/Features/Home/wedgits/custom_text_form_field.dart
+++ lib/Features/Home/wedgits/custom_text_form_field.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField(
-      {super.key,
-      required this.hint,
-      this.maxlines = 1,});
+  const CustomTextFormField({
+    super.key,
+    required this.hint,
+    this.maxlines = 1,
+    required this.onSaved,
+  });
   final String hint;
   final int maxlines;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10),
       child: TextFormField(
+        onSaved: onSaved,
         validator: (value) {
           if (value?.isEmpty ?? true) {
             return "Field is Required";
