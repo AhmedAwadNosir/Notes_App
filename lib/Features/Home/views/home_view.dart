faa5dd054f15a25d55953574c81ffe7648fc9ee1
AhmedAwadNosir <ahmedalprens98@gmail.com>
2023-05-16 00:58:30 +0300

    make bottom shild scrollable and mak it move to top when kypword show

--- lib/Features/Home/views/home_view.dart
+++ lib/Features/Home/views/home_view.dart
@@ -14,6 +14,7 @@ class HomeView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           showModalBottomSheet(
+            isScrollControlled: true,
             context: context,
             backgroundColor: AppColors.primaryColor,
             builder: (context) {
--- lib/Features/Home/wedgits/adding_note_bottom_sheet.dart
+++ lib/Features/Home/wedgits/adding_note_bottom_sheet.dart
@@ -9,54 +9,60 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const CustomTextFormField(
-          hint: "learn Flutter",
-        ),
-        const CustomTextFormField(
-          hint: "learn Flutter With tharwat samy",
-          maxlines: 5,
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const [
-              ColorPiker(color: Colors.white),
-              ColorPiker(color: Colors.red),
-              ColorPiker(color: Colors.lightBlue),
-              ColorPiker(color: Colors.green),
-            ],
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.cyan,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10))),
-              child: const Padding(
-                padding: EdgeInsets.all(7),
-                child: Text(
-                  "Add",
-                  style: TextStyle(fontSize: 30, color: Colors.black),
-                ),
+    return Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const CustomTextFormField(
+              hint: "learn Flutter",
+            ),
+            const CustomTextFormField(
+              hint: "learn Flutter With tharwat samy",
+              maxlines: 5,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: const [
+                  ColorPiker(color: Colors.white),
+                  ColorPiker(color: Colors.red),
+                  ColorPiker(color: Colors.lightBlue),
+                  ColorPiker(color: Colors.green),
+                ],
               ),
             ),
-          ),
-        )
-      ],
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.cyan,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  child: const Padding(
+                    padding: EdgeInsets.all(7),
+                    child: Text(
+                      "Add",
+                      style: TextStyle(fontSize: 30, color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
