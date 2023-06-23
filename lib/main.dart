f59c1ef0f1267b6be6d98422c8f9e7561eb9db4e
AhmedAwadNosir <ahmedalprens98@gmail.com>
2023-06-23 23:19:43 +0300

    create customblocobserver to trace states of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app_git/Features/Home/views/edit_note_view.dart';
 import 'package:notes_app_git/Features/home/views/home_view.dart';
 import 'package:notes_app_git/Models/note_model.dart';
+import 'package:notes_app_git/Utils/custom_bloc_observer.dart';
 
 import 'Utils/app_colors.dart';
 
@@ -10,7 +12,7 @@ void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(NoteModelAdapter());
   Hive.openBox<NoteModel>("notes");
-
+  Bloc.observer = CustomBlocObserver();
   runApp(const NotesApp());
 }
 
